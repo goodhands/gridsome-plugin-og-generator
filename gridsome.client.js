4ca9cc12a0526f69bc2b2abce5aa8b7d7190f2a0
Samuel Olaegbe <olaegbesamuel@gmail.com>
2020-10-15 11:18:02 +0100

    wip

--- gridsome.client.js
+++ gridsome.client.js
@@ -1,19 +1,59 @@
-import htmlToImage from 'html-to-image';
+import * as htmlToImage from 'html-to-image';
 
 export default function (Vue, context, { head }) {
     Vue.mixin({
-        created: function () {
-          var myOption = this.$options.myOption
-          if (myOption) {
-            console.log(myOption)
-          }
+        data() {
+            return {
+                post: null,
+                origin: document.location.origin,
+            }
         },
         methods: {
             OgGenerator(post){
-                head.meta.push({
-                    name: "title",
-                    content: post.title
-                });
+                this.post = post;
+                this.buildCards().map(meta => {
+                    head.meta.push(meta);
+                })
+            },
+
+            buildCards(){
+                return [
+                    {
+                        name: "og:title",
+                        content: this.post.title
+                    },
+                    {
+                        name: "og:type",
+                        content: "article"
+                    },
+                    {
+                        name: "og:url",
+                        content: this.origin + this.post.path
+                    },
+                    {
+                        name: "og:image",
+                        content: this.origin + this.post.cover.src
+                    },
+                ]
+            },
+
+            oGImage(){
+                // const markup = `
+                //     <div class="bg-primary border-15 relative border-solid border-solidblue px-8 flex flex-row items-center justify-evenly py-8">
+                //         <div class="post-title flex flex-col">
+                //             <span class="font-body italic text-tiny">${this.post.category.title}</span>
+                //             <h2 class="font-bold leading-tight text-2xl">
+                //                 ${this.post.title}
+                //             </h2>
+                //             <div class="absolute bottom-0 mb-4">
+                //                 <h2>By Samuel Olaegbe</h2>
+                //                 <p>https://goodhands.github.io</p>
+                //             </div>
+                //         </div>
+                //         <g-image src="~/media/Sam.jpg" class="h-48 rounded-full w-48"></g-image>
+                //     </div>
+                //     `;
+
             }
         },
     })
