3fec7ea51b3aec3e02681a590ce9ebf3382eb8b5
Samuel Olaegbe <olaegbesamuel@gmail.com>
2020-10-15 21:28:49 +0100

    convert html to image with puppeteer

--- gridsome.client.js
+++ gridsome.client.js
@@ -1,9 +1,9 @@
+
 export default function (Vue, context, { head }) {
     Vue.mixin({
         data() {
             return {
                 post: null,
-                origin: document.location.origin,
             }
         },
         methods: {
@@ -24,33 +24,33 @@ export default function (Vue, context, { head }) {
                         name: "og:type",
                         content: "article"
                     },
-                    {
-                        name: "og:url",
-                        content: this.origin + this.post.path
-                    },
-                    {
-                        name: "og:image",
-                        content: this.origin + this.post.cover.src
-                    },
+                    // {
+                    //     name: "og:url",
+                    //     content: document.location.origin + this.post.path
+                    // },
+                    // {
+                    //     name: "og:image",
+                    //     content: document.location.origin + this.post.cover.src
+                    // },
                 ]
             },
 
             oGImage(){
-                // const markup = `
-                //     <div class="bg-primary border-15 relative border-solid border-solidblue px-8 flex flex-row items-center justify-evenly py-8">
-                //         <div class="post-title flex flex-col">
-                //             <span class="font-body italic text-tiny">${this.post.category.title}</span>
-                //             <h2 class="font-bold leading-tight text-2xl">
-                //                 ${this.post.title}
-                //             </h2>
-                //             <div class="absolute bottom-0 mb-4">
-                //                 <h2>By Samuel Olaegbe</h2>
-                //                 <p>https://goodhands.github.io</p>
-                //             </div>
-                //         </div>
-                //         <g-image src="~/media/Sam.jpg" class="h-48 rounded-full w-48"></g-image>
-                //     </div>
-                //     `;
+                const markup = `
+                    <div class="bg-primary border-15 relative border-solid border-solidblue px-8 flex flex-row items-center justify-evenly py-8">
+                        <div class="post-title flex flex-col">
+                            <span class="font-body italic text-tiny">${this.post.category.title}</span>
+                            <h2 class="font-bold leading-tight text-2xl">
+                                ${this.post.title}
+                            </h2>
+                            <div class="absolute bottom-0 mb-4">
+                                <h2>By Samuel Olaegbe</h2>
+                                <p>https://goodhands.github.io</p>
+                            </div>
+                        </div>
+                        <g-image src="~/media/Sam.jpg" class="h-48 rounded-full w-48"></g-image>
+                    </div>
+                    `;
 
             }
         },
